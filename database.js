7225b92cfd00625afd90a9e2b639c84b057d5bca
Rafael Matheus <rafaelmat142@gmail.com>
2025-09-22 02:34:25 -0300

    feat: changing database for postgres

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "database.db"
-});
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+});
 module.exports = sequelize;
